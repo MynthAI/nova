77ad418c4e9728b2b4cf7ce8e8991f4ea4ab4516
SynthLuvr <131367121+SynthLuvr@users.noreply.github.com>
2026-02-19 08:34:36 +0100

    Allow configuration override

--- config.ts
+++ config.ts
@@ -1,6 +1,9 @@
+import { logExit } from "cli";
 import Conf from "conf";
 import { createHash, randomBytes } from "crypto";
+import { existsSync, statSync } from "fs";
 import { getMachineIdSync } from "native-machine-id";
+import { resolve } from "path";
 import { Network } from "./endpoints";
 
 type Settings = {
@@ -21,11 +24,35 @@ const getEncryptionKey = () =>
     .update(getMachineIdSync() ?? defaultId, "hex")
     .digest("hex");
 
-const config = new Conf<Settings>({
-  clearInvalidConfig: true,
-  encryptionKey: getEncryptionKey(),
-  projectName: "auth",
-});
+const getConfigCwd = (): string | undefined => {
+  const value = process.env.NOVA_CONFIG?.trim();
+  if (!value) return undefined;
+
+  try {
+    const fullPath = resolve(value);
+    if (existsSync(fullPath) && statSync(fullPath).isDirectory())
+      return fullPath;
+  } catch {}
+
+  logExit("NOVA_CONFIG is not a valid directory path");
+  process.exit(1);
+};
+
+const config = (() => {
+  const cwd = getConfigCwd();
+
+  try {
+    return new Conf<Settings>({
+      clearInvalidConfig: true,
+      cwd,
+      encryptionKey: getEncryptionKey(),
+      projectName: "auth",
+    });
+  } catch {
+    logExit(`${cwd} isn't writable`);
+    process.exit(1);
+  }
+})();
 
 const getNetwork = () => config.get("network") ?? "testnet";
 
